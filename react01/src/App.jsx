e08dd7a13510a83a49dbe495611713627bb690dc
Prajyot Patil <prajyotbpatil2744@gmail.com>
2024-07-31 18:57:56 +0530

    react learning

---
+++ react01/src/App.jsx
@@ -0,0 +1,9 @@
+
+function App() {
+  
+  return (
+   <h1>This is Project | ReactJs</h1>
+  )
+}
+
+export default App
---
+++ react02/src/App.jsx
@@ -0,0 +1,36 @@
+import './App.css'
+import { useState } from 'react'
+
+
+function App() {
+  let [counter , setcounter] = useState(0)
+
+  const addValue = () =>{
+    if(counter < 20){
+     setcounter(counter + 1) 
+    }
+    
+  }
+
+  const removeValue = () => {
+    if(counter > 0){
+      setcounter(counter - 1);
+    }
+    
+  }
+  
+
+  return (
+    <div className='container'>
+      <h3>This is First React Code</h3>
+      <h1>Number Counter</h1>
+      <p>{counter}</p>
+      <br />
+      <button onClick={addValue}>{counter} Add Value</button>
+      <br />
+      <button onClick={removeValue}>{counter} Remove Value</button>
+    </div>   
+  )
+}
+
+export default App
