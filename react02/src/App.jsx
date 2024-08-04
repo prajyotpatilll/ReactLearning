b2cc2b95d46bc7f7830785323fc9de45b6110439
Prajyot Patil <prajyotbpatil2744@gmail.com>
2024-08-04 11:08:20 +0530

    add

--- react02/src/App.jsx
+++ react02/src/App.jsx
@@ -1,27 +1,28 @@
-import './App.css'
-import { useState } from 'react'
-
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  let [counter , setcounter] = useState(0)
+  let [counter, setcounter] = useState(0);
 
-  const addValue = () =>{
-    if(counter < 20){
-     setcounter(counter + 1) 
-    }
-    
-  }
+  const addValue = () => {
+    if (counter < 20) {
+      setcounter(counter + 1);
+      // setcounter(prevCounter => prevCounter + 1);
+      // // setcounter(prevCounter => prevCounter + 1);
+      // // setcounter(prevCounter => prevCounter + 1);
+      // // setcounter(prevCounter => prevCounter + 1);
+      // // setcounter(prevCounter => prevCounter + 1);
+      
+  };
 
   const removeValue = () => {
-    if(counter > 0){
+    if (counter > 0) {
       setcounter(counter - 1);
     }
-    
-  }
-  
+  };
 
   return (
-    <div className='container'>
+    <div className="container">
       <h3>This is First React Code</h3>
       <h1>Number Counter</h1>
       <p>{counter}</p>
@@ -29,8 +30,8 @@ function App() {
       <button onClick={addValue}>{counter} Add Value</button>
       <br />
       <button onClick={removeValue}>{counter} Remove Value</button>
-    </div>   
-  )
+    </div>
+  );
 }
-
-export default App
+}
+export default App;
